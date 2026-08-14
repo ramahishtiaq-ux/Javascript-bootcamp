95302a9d03a6cd458602996370f7fd9bd81c13a1
ramahishtiaq-ux <ramahishtiaq@gmail.com>
2026-08-14 13:43:39 +0500

    Add JavaScript practice

---
+++ app1.js
@@ -0,0 +1,42 @@
+console.log("Hello World");
+
+console.log(5+5);
+
+console.log(5-5);
+
+
+let a = 10;
+let b = 5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+console.log(10>5);
+console.log(10<5);
+console.log(10===10);
+console.log(10==="5");
+console.log(10===5);
+console.log(10!==5);
+console.log(10 !==10);
+
+
+// logical operators
+
+let age = 20;
+
+console.log(age > 18 && age < 60); //true
+console.log(age < 10 || age > 18);
+console.log(!(age > 18));
+
+
+let number = -5;
+
+if(number > 0){
+    console.log("Positive number");
+}
+else {
+    console.log("Negative Number");
+}
