ffac1e38ec95cec01890eac8f17403efd8a068da
RAMAH ISHTIAQ <ramahishtiaq@gmail.com>
2026-08-19 18:52:48 +0500

    Refactor app.js with variable updates and loops
    Refactor variable declarations and add loops for grade calculation.

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 console.log("Hello world");
-console.log(5+5);
 
+console.log(5 + 5);
 
+let name = "Ramah Ishtiaq";
 
-
-let name = "Ramah Ishtiaq"
 let age = 21;
-age =22;
-const pi =3.14;
-pi = 2020;
-let isstudent = true;
+age = 22;
+
+const pi = 3.14;
+
+let isStudent = true;
 
 console.log(age);
 console.log(pi);
+
+// For loop
+for (let i = 1; i <= 12; i++) {
+    console.log(i);
+}
+
+// Grade loop
+for (let i = 1; i <= 5; i++) {
+    let marks = i * 20;
+
+    if (marks >= 80) {
+        console.log(marks + " Grade A");
+    } else if (marks >= 60) {
+        console.log(marks + " Grade B");
+    }
+}
